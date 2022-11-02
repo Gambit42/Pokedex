4a77f885091f57bd50105236a12f804f126324d1
Prins Ocampo <85613393+Gambit42@users.noreply.github.com>
2022-11-02 15:53:24 +0800

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,16 +6,6 @@ const Home = () => {
   TabTitle("Pokedex");
   return (
     <div className="pb-4 px-2 bg-stone-50 pt-16 min-h-screen">
-     <h1
-        onClick={() => {
-          window.open(
-            "https://www.facebook.com",
-            "_blank",
-            "width=500,height=500"
-          );
-        }}>
-        TEST ON MOBILE
-      </h1>
       <PokemonListContainer />
     </div>
   );
